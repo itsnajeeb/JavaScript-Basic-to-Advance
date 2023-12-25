016a9814aa22dc57c5103af66c0589342d38ad97
Md Najeebur Rahman <125728005+itsnajeeb@users.noreply.github.com>
2023-12-25 16:01:06 +0000

    Cover Another Topic of JS Array

---
+++ 02_basic/02_array.js
@@ -0,0 +1,26 @@
+// console.log(Array.isArray("Najeeb"));
+
+// let NewArr=Array.from("Najeeb")
+// NewArr.push(10)
+// console.log(NewArr)
+
+// let arr1=[10,20,30,40,50];
+// let arr2=[0,1,2,3,4,5]
+// let arr3=[90,100,120,130]
+// let arr4=["Najeb"];
+// let newArr=arr4.concat(arr1,arr2,arr3)
+// console.log(newArr);
+
+// let arr=[10,20,30,[30,40,50], 0,1,2,3,[100,200,300,[200,300,400,[10,20,30]]]]
+// let newArr=arr.flat(Infinity)
+// console.log(newArr);
+
+// console.log(Array.from({name:"Najeeb"}));
+
+let score1=100;
+let score2=200;
+let score3=300;
+
+console.log(Array.of(score1,score2,score3));
+
+
