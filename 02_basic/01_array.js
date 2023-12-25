29870821a7024250076dbaac7c1a29e11d358e89
Md Najeebur Rahman <125728005+itsnajeeb@users.noreply.github.com>
2023-12-25 15:33:34 +0000

    Cover Basic Js Array Part-1

---
+++ 02_basic/01_array.js
@@ -0,0 +1,40 @@
+const myArr=[0,1,2,3,4,5];
+
+// console.log(myArr.length);
+
+
+// console.log(myArr.toString());
+
+
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.push(10,20,30)
+// console.log(myArr);
+
+// myArr.shift()
+// console.log(myArr);
+
+// myArr.unshift(10,20,30)
+// console.log(myArr);
+
+// let newArr=myArr.join('-')
+// console.log( newArr);
+
+// let arr1=[10,20,30];
+// let arr2=[30,40,50,60]
+// let newArr=myArr.concat(arr1,arr2)
+// console.log(newArr)
+// console.log(newArr[10]);
+
+// let sliceArr=myArr.slice(0,3)
+// let sliceArr1=myArr.slice(4)
+// console.log( sliceArr);
+// console.log( sliceArr1);
+// console.log(myArr);
+
+// let spliceArr=myArr.splice(0,2)
+// console.log(spliceArr);
+// console.log(myArr);
+
+myArr.flat()
