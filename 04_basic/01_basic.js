0093052444c69bb208cc7c1b0edc0396697665c9
Md Najeebur Rahman <125728005+itsnajeeb@users.noreply.github.com>
2024-01-01 06:40:07 +0000

    switch, if_elase and operator

---
+++ 04_basic/01_basic.js
@@ -0,0 +1,26 @@
+// let userLoggedIn=true;
+// if(userLoggedIn){
+//     console.log("User Logged In");
+// }
+// else{
+//     console.log("User Not Logged In");
+// }
+
+// let userEmail="najeeb@hotmail.com";
+// let userEmail="";
+// let userEmail=[]
+// let userEmail=null;
+// if(userEmail){
+//     console.log("Email : " + userEmail);
+// }
+// else{
+//     console.log("Email not Found ");
+// }
+// turnery operator 
+
+// let value=100;
+// value>200 ? console.log(`${value} is greather `) : console.log(add());;
+// function add(){
+//     return 10+20;
+// }
+
