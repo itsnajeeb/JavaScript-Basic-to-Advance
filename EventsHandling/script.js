5e7f3999abaeb34ec71f3df9d6aeaa7f65b5bf8f
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-18 00:18:59 +0530

    Keydown Event

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,24 +1,10 @@
-let p = document.querySelector('p')
-let dbClick = () => {
-    p.style.color = 'yellow'
-}
-let IncreaseFontSize = () => {
-    p.style.fontSize = '24px'
-}
-// p.addEventListener('dblclick',dbClick)
-// p.addEventListener('click',IncreaseFontSize)
-// p.removeEventListener('dblclick',dbClick)
+let heading = document.querySelector('h1')
+window.addEventListener('keydown', function (dets) {
+    if (dets.key != " ")
+        heading.textContent = dets.key
+    else
+        heading.textContent = "SPACE"
 
-let inp = document.querySelector('input')
-inp.addEventListener('input', function (dets) {
-    // console.log(dets.data);
-})
 
-let select = document.querySelector('select');
-let device = document.querySelector('#device')
-select.addEventListener('change', function(dets){
-    let selectedDevice=dets.target.value;
-        
-    device.textContent= "You Selected " + `${!selectedDevice ? `Value is Empty`  : selectedDevice}`
-    
+
 })
