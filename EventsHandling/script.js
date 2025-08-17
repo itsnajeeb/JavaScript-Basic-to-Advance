75fa157ddacd8cd53cb8209322e9065256436d72
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-18 00:59:45 +0530

    Change input file style with button

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,10 +1,15 @@
-let heading = document.querySelector('h1')
-window.addEventListener('keydown', function (dets) {
-    if (dets.key != " ")
-        heading.textContent = dets.key
-    else
-        heading.textContent = "SPACE"
+let btn = document.querySelector('#btn')
+let fileInp = document.querySelector('#fileInp');
 
-
-
+btn.addEventListener('click', (dets) => {
+    fileInp.click()
 })
+
+
+fileInp.addEventListener('change', (dets) => {
+    let file = dets.target.files[0];
+    if (file)
+        btn.innerText = file.name
+})
+
+
