fe0b52093eeaacef76ef6019c7c5d3dd4fc7ec90
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-18 02:32:18 +0530

    Creating a form and adding form data into dynamic creted card

--- EventsHandling/script.js
+++ EventsHandling/script.js
@@ -1,15 +1,38 @@
-let btn = document.querySelector('#btn')
-let fileInp = document.querySelector('#fileInp');
+let inputData = document.querySelectorAll('input')
+let form = document.querySelector('form')
+let main = document.querySelector('#main')
 
-btn.addEventListener('click', (dets) => {
-    fileInp.click()
+
+form.addEventListener('submit', function (dets) {
+    dets.preventDefault()
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let profile = document.createElement('div');
+    profile.classList.add('profile')
+
+    let img = document.createElement('img')
+    img.setAttribute('src', inputData[0].value)
+    profile.appendChild(img)
+
+    let h2 = document.createElement('h2')
+    h2.textContent = inputData[1].value
+    let h3 = document.createElement('h3')
+    h3.textContent = inputData[2].value
+    let p = document.createElement('p')
+    p.textContent = inputData[3].value
+
+    card.appendChild(profile);
+
+    card.appendChild(h2)
+    card.appendChild(h3)
+    card.appendChild(p)
+
+    main.appendChild(card)
+
+    inputData[0].value = ""
+    inputData[1].value = ""
+    inputData[2].value = ""
+    inputData[3].value = ""
+
 })
-
-
-fileInp.addEventListener('change', (dets) => {
-    let file = dets.target.files[0];
-    if (file)
-        btn.innerText = file.name
-})
-
-
