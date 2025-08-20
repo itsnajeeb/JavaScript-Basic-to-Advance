430581925f74fcdc84758caeca4873ad1617b9f8
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-20 12:06:10 +0530

    Basic Concept of localStorage

---
+++ localStorage/script.js
@@ -0,0 +1,20 @@
+//localStorage ->
+    // A key-value storage built into browser 
+    // saves data persistently (even after reload or browser restart)
+    // stores only string (but you can JSON.stringfy Object / array)
+
+//Store / save the data 
+localStorage.setItem('Name', "Najeeb")
+localStorage.setItem('Profession', "Programmer")
+
+// Remove 
+// localStorage.removeItem('Name')
+
+//Update 
+localStorage.setItem('Profession', "Software Engineer")
+
+//Get 
+let item =localStorage.getItem('Profession',)
+console.log(item);
+
+
