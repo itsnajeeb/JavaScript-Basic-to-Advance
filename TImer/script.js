10610d4670e0e51efafe441344f4053ff81dc159
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-20 11:39:02 +0530

    Downlaoded progress bar with countdown

--- TImer/script.js
+++ TImer/script.js
@@ -1,16 +1,29 @@
 let progress = document.querySelector('.progress-bar');
-let percent = document.querySelector('.percent')
-let heading = document.querySelector('h2')
+let percent = document.querySelector('.percent');
+let heading = document.querySelector('h2');
+let span = document.querySelector('h4 span');
+console.dir(span);
+
+let counter = 10;
 let count = 0;
-let intv = setInterval(() => {
-    if (count <= 99) {
-        count++
-        progress.style.width = `${count}%`
-        percent.textContent= `${count}%`
+
+let outer = setInterval(() => {
+    progress.style.width = `0%`
+    if (counter >= 0) {
+        span.textContent = counter; // optional display
+        counter--;
+    } else {
+        clearInterval(outer); // stop countdown
+
+        let intv = setInterval(() => {
+            if (count <= 99) {
+                count++;
+                progress.style.width = `${count}%`;
+                percent.textContent = `${count}%`;
+            } else {
+                heading.textContent = "Downloaded";
+                clearInterval(intv);
+            }
+        }, 50);
     }
-   
-    else{
-        heading.textContent="Downloaded"
-        clearInterval(intv)
-    }
-}, 50);
+}, 1000);
