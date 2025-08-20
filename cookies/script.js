9b07ff49716ee5a5d5e7fab712d19aa91dd62c98
Md Najeebur Rahman <iamnajeebjung@gmail.com>
2025-08-20 13:19:41 +0530

    Added data into Cookies

---
+++ cookies/script.js
@@ -0,0 +1,11 @@
+//Cookies
+    //A baked sweet (like chocolate chip cookies).
+    // A web browser "cookie" (small piece of data stored on your computer by websites).
+    // A playful expression (like calling someone a "smart cookie").
+
+    // document.cookie = 'name=najeeb'
+    // document.cookie = 'email=iamnajeebjung.com'
+
+
+
+
